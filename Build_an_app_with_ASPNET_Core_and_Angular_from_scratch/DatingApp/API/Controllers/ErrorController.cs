34516d897418c945ce4c676bf24e8159d1a14a6a
ombhamare4 <om.rubiks@gmail.com>
2024-08-08 21:55:57 +0530

    Section 7 started

---
+++ Build_an_app_with_ASPNET_Core_and_Angular_from_scratch/DatingApp/API/Controllers/ErrorController.cs
@@ -0,0 +1,45 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+namespace API.Controllers;
+
+public class ErrorController(DataContext context) : BaseApiController
+{
+    [HttpGet("auth")]
+    public ActionResult<string> GetAuth()
+    {
+        return "secrest test";
+    }
+
+    [HttpGet("not-found")]
+    public ActionResult<AppUser> GetNotFound()
+    {
+        var things = context.Users.Find(-1);
+        if (things == null)
+        {
+            return NotFound();
+        }
+
+        return things;
+    }
+
+    [HttpGet("server-error")]
+    public ActionResult<string> GetServerError()
+    {
+        var thing = context.Users.Find(-1).ToString();
+
+        return "secrest test";
+    }
+
+    [HttpGet("auth")]
+    public ActionResult<string> GetAuth()
+    {
+        return "secrest test";
+    }
+
+    [HttpGet("auth")]
+    public ActionResult<string> GetAuth()
+    {
+        return "secrest test";
+    }
+}
