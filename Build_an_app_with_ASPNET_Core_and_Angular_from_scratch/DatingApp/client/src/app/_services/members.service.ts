83f1b87653be210ab24b3f35b2535fab9453f74b
ombhamare4 <om.rubiks@gmail.com>
2024-09-18 09:31:08 +0530

    Section 10: Ended

--- Build_an_app_with_ASPNET_Core_and_Angular_from_scratch/DatingApp/client/src/app/_services/members.service.ts
+++ Build_an_app_with_ASPNET_Core_and_Angular_from_scratch/DatingApp/client/src/app/_services/members.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/Member';
+import { of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -19,11 +20,18 @@ export class MembersService {
     });
   }
   getMember(username: string) {
-    const member = this.members().find
+    const member = this.members().find((x) => x.username === username);
+    if (member !== undefined) return of(member);
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
-  updateMember(memeber: Member) {
-    return this.http.put(this.baseUrl + 'users', memeber);
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      tap(() => {
+        this.members.update((members) =>
+          members.map((x) => (x.username === member.username ? member : x))
+        );
+      })
+    );
   }
 }
